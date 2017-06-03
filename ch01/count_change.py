6441d4657b3b9de0e75daf2cc1ec73e674ff23bc
ademyanchuk <alexeydemyanchuk@gmail.com>
2017-06-03 20:50:24 +0700

    count_change tree recursion

---
+++ ch01/count_change.py
@@ -0,0 +1,27 @@
+#! /usr/local/bin/python3
+# count_change is another example of tree recursion
+
+def count_change(amount):
+    return cc(amount, 5)
+
+def cc(amount, kinds_of_coins):
+    if amount == 0:
+        return 1
+    elif amount < 0 or kinds_of_coins == 0:
+        return 0
+    else:
+        return cc(amount, kinds_of_coins-1) + cc(amount-first_denmination(kinds_of_coins),kinds_of_coins)
+
+def first_denmination(kinds_of_coins):
+    if kinds_of_coins == 1:
+        return 1
+    elif kinds_of_coins == 2:
+        return 5
+    elif kinds_of_coins == 3:
+        return 10
+    elif kinds_of_coins == 4:
+        return 25
+    elif kinds_of_coins == 5:
+        return 50
+
+print(count_change(100))
