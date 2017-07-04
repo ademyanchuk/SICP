3bdceacfd59bf148edd6f2268ac546bcd81ebae1
ademyanchuk <alexeydemyanchuk@gmail.com>
2017-07-04 16:57:28 +0700

    exx 1.22 - 1.24 (speed test for primality test)

--- ch01/smallest_divisor.py
+++ ch01/smallest_divisor.py
@@ -4,13 +4,20 @@ import time
 def  smallest_divisor(n):
     return find_divisor(n, 2)
 
+def next_divisor(test_divisor):
+    if test_divisor == 2:
+        return 3
+    else:
+        return test_divisor + 2
+
 def find_divisor(n, test_divisor):
+    while (test_divisor**2 > n ==False or divides(test_divisor, n) == False):
+        test_divisor = next_divisor(test_divisor)
+
     if test_divisor**2 > n:
         return n
-    elif divides(test_divisor, n):
-        return test_divisor
     else:
-        return find_divisor(n, test_divisor + 1)
+        return test_divisor
 
 def divides(a,b):
     return b%a == 0
@@ -18,15 +25,20 @@ def divides(a,b):
 def isPrime(n):
     return smallest_divisor(n) == n
 
-def timedPrimeTest(n):
-    print("\n")
-    print(n)
-    return startPrimeTest(n, round(time.time() * 1000000))
+def search_primes(start, stop):
+    count = 0
+    for n in range(start, stop):
+        if count == 3:
+            break
+        if isPrime(n) and n%2 != 0:
+            print(n)
+            count+=1
+    return None
 
-def startPrimeTest(n, start_time):
-    if isPrime(n):
-        return reportPrime(round(time.time() * 1000000) - start_time)
-
-def reportPrime(elapsed_time):
-    print(" *** ")
-    print(elapsed_time)
+####
+# Measurements
+###
+# %timeit(isPrime(1009)) ---- 344 µs ± 5.66 µs per loop
+# %timeit(isPrime(10007)) ---- 3.59 ms ± 48.2 µs per loop
+# %timeit(isPrime(100003)) ---- 35.9 ms ± 775 µs per loop
+# %timeit(isPrime(1000003)) ---- 346 ms ± 3.52 ms per loop
--- ch01/test_fermat.py
+++ ch01/test_fermat.py
@@ -29,3 +29,10 @@ def isPrime_fast(n, times):
     else:
         return False
 
+####
+# Measurements
+###
+# %timeit(isPrime_fast(1009, 10)) -- 113 µs ± 340 ns per loop
+# %timeit(isPrime_fast(10007, 10)) -- 148 µs ± 1.79 µs per loop
+# %timeit(isPrime_fast(100003, 10)) -- 177 µs ± 2.41 µs per loop
+# %timeit(isPrime_fast(1000003, 10)) -- 205 µs ± 4.5 µs per loop
